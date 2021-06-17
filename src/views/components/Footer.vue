51d740dab210e2d53367fde0d3da51869cc53e88
andy <admazzola@gmail.com>
2021-06-17 15:23:47 -0400

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -2,14 +2,14 @@
   <div class="section bg-gray-900 text-white">
     <div class="container-3 w-container">
       <div class="flex flex-row px-2 py-8 pb-8">
-        <div class="flex flex-col w-1/3">
+        <div class="flex flex-col w-1/2">
           <h3 class="text-lg font-bold py-2">Documentation</h3>
 
            <a href="https://etherscan.io/address/0x216d7636b791Ed93BFD56BF696b35A14115416ab " target="_blank" class="footer-link">Token Contract</a>
         <a href="https://github.com/birbtoken " target="_blank" class="footer-link">BIRB Github</a>
 
         </div>
-        <div class="flex flex-col w-1/3">
+        <div class="flex flex-col w-1/2">
           <h3 class="text-lg font-bold py-2">Community &amp; Social</h3>
           
           <a href="https://www.reddit.com/r/birbtoken/" target="_blank" class="footer-link">Reddit</a>
@@ -22,10 +22,7 @@
 
 
           </div>
-        <div class="flex flex-col hidden">
-          <h3 class="text-lg font-bold py-2">Fun &amp; Learning</h3>
-          
-        </div>
+       
 
       
     </div>
